e4f485d1420af2c88f148a9b095490255c60736d
Miro <45430033+HotelEnglish@users.noreply.github.com>
2024-12-22 19:38:30 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -17,7 +17,7 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_URL: '#', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  STARTER_HERO_PREVIEW_IMAGE: '/images/home.png', // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_PREVIEW_IMAGE: '/images/home.jpg', // 产品预览图 ，默认读取public目录下图片
   STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
   // 顶部右侧导航暗流
   STARTER_NAV_BUTTON_1_TEXT: '了解智意达',
