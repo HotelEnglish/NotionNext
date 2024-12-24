70a61205e31e5ec7e4be7aa293638aa6059c5fff
Miro <45430033+HotelEnglish@users.noreply.github.com>
2024-12-24 14:16:09 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -170,12 +170,44 @@ const CONFIG = {
   STARTER_CONTACT_LOCATION_TEXT: '中国，海南',
   STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
   STARTER_CONTACT_EMAIL_TEXT: 'admin#begs.cn',
+  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7'
   // ... (联系模块其他项)
 
   // 合作伙伴的图标
   STARTER_BRANDS_ENABLE: true, // 合作伙伴开关
   // ... (合作伙伴项)
-
+  STARTER_BRANDS: [
+    {
+      IMAGE: '/images/starter/brands/graygrids.svg',
+      IMAGE_WHITE: '/images/starter/brands/graygrids-white.svg',
+      URL: 'https://graygrids.com/',
+      TITLE: 'graygrids'
+    },
+    {
+      IMAGE: '/images/starter/brands/lineicons.svg',
+      IMAGE_WHITE: '/images/starter/brands/lineicons-white.svg',
+      URL: 'https://lineicons.com/',
+      TITLE: 'lineicons'
+    },
+    {
+      IMAGE: '/images/starter/brands/uideck.svg',
+      IMAGE_WHITE: '/images/starter/brands/uideck-white.svg',
+      URL: 'https://uideck.com/',
+      TITLE: 'uideck'
+    },
+    {
+      IMAGE: '/images/starter/brands/ayroui.svg',
+      IMAGE_WHITE: '/images/starter/brands/ayroui-white.svg',
+      URL: 'https://ayroui.com/',
+      TITLE: 'ayroui'
+    },
+    {
+      IMAGE: '/images/starter/brands/tailgrids.svg',
+      IMAGE_WHITE: '/images/starter/brands/tailgrids-white.svg',
+      URL: '"https://tailgrids.com/',
+      TITLE: 'tailgrids'
+    }
+  ],
   STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
   // ... (页脚其他项)
 
@@ -196,4 +228,48 @@ const CONFIG = {
   STARTER_POST_REDIRECT_URL: 'https://zsk.begs.cn/chat/share?shareId=ie34m7dyg5em9hktjcb04kqf', // 重定向域名
   STARTER_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_STARTER_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
+
+
+  STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+
+  // 页脚三列菜单组
+  STARTER_FOOTER_LINK_GROUP: [
+    {
+      TITLE: '关于我们',
+      LINK_GROUP: [
+        { TITLE: '官方主页', URL: '/#home' },
+        { TITLE: '操作文档', URL: 'https://doc.hotelcases.cn' },
+        { TITLE: '帮助支持', URL: 'https://help.hotelcases.cn' },
+        { TITLE: '合作申请', URL: 'https://co.hotelcases.cn' }
+      ]
+    },
+    {
+      TITLE: '功能特性',
+      LINK_GROUP: [
+        { TITLE: '智能助理', URL: 'https://zsk.begs.cn/chat/share?shareId=vkxiuc2zhuoxedqecc6ovmwu' },
+        { TITLE: '政策专家', URL: 'https://zsk.begs.cn/chat/share?shareId=ie34m7dyg5em9hktjcb04kqf' },
+        { TITLE: '收益管理', URL: 'https://zsk.begs.cn/chat/share?shareId=augem081tlh7y82lcr8dxxuc' }
+      ]
+    },
+    {
+      TITLE: 'Notion写作',
+      LINK_GROUP: [
+        { TITLE: '客房服务', URL: '#' },
+        { TITLE: '前厅服务', URL: '#' },
+        { TITLE: '餐饮服务', URL: '#' }
+      ]
+    }
+  ],
+
+  STARTER_FOOTER_BLOG_LATEST_TITLE: '最新动态',
+
+  STARTER_FOOTER_PRIVACY_POLICY_TEXT: '隐私政策',
+  STARTER_FOOTER_PRIVACY_POLICY_URL: '/privacy-policy',
+
+  STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: '法律声明',
+  STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_URL: '/legacy-notice',
+
+  STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: '服务协议',
+  STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_URL: '/terms-of-use',
+
 export default CONFIG;
