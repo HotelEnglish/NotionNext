e6d6823b4bd94b17e2cabb0411f0d636687b0c93
Miro <45430033+HotelEnglish@users.noreply.github.com>
2025-04-05 22:42:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '龚老师', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个需要草料的牛马 🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.begs.cn', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.miro.cx', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '网络科技, 技术博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
